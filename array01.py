a9d07225af34d6eaa439a3dcb546466d0d7090a9
Manoranjan <manojsmrt108@gmail.com>
2025-11-11 09:05:48 +0530

    max value in array

--- array01.py
+++ array01.py
@@ -1,1 +1,8 @@
-print('hello')
+#find out largest element in array
+
+givenArray=[2,14,6,8,10]
+maxNumber=2
+for num in range(len(givenArray)):
+    if  maxNumber<givenArray[num]:
+     maxNumber=givenArray[num]
+    print("MAX Number is : ",maxNumber)
